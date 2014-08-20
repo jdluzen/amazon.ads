eb98d338e414687d2590e089ea2040b796812fad
Joe Dluzen <jdluzen@gmail.com>
2014-08-20 19:49:34 -0400

    Initial commit

---
+++ Amazon.Ads/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Amazon.Ads")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("5.4.78")]
