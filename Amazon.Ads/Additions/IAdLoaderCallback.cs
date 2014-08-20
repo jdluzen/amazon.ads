426ef40f7a3bf6565616672f8669e7a379b0c1cf
Joe Dluzen <160958+jdluzen@users.noreply.github.com>
2014-08-20 19:49:34 -0400

    Initial commit

---
+++ Amazon.Ads/Additions/IAdLoaderCallback.cs
@@ -0,0 +1,13 @@
+﻿namespace Com.Amazon.Device.Ads
+{
+    public interface IAdLoaderCallback
+    {
+        void HandleResponse();
+
+        void AdFailed(AdError p0);
+
+        void AdShown();
+
+        int Timeout { get; }
+    }
+}
